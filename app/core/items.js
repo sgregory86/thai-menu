bf956b6a35b85655393c649584c00bc45492a735
unknown <sgregory@gmail.com>
2014-11-17 08:05:51 -0600

    Removed unneeded KO dependency

--- app/core/items.js
+++ app/core/items.js
@@ -1,18 +1,16 @@
-define([
-    'knockout'
-], function(ko) {
+define(function() {
     'use strict';
 
-        var items = [{
-            name: "Tom Yum Goong",
-            price: "5.99"
-        }, {
-            name: "Soft Spring Rolls",
-            price: "4.99"
-        }, {
-            name: "Pad Thai",
-            price: "8.99"
-        }];
+    var items = [{
+        name: "Tom Yum Goong",
+        price: "5.99"
+    }, {
+        name: "Soft Spring Rolls",
+        price: "4.99"
+    }, {
+        name: "Pad Thai",
+        price: "8.99"
+    }];
 
     return items;
 });
