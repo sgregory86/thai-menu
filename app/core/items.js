8edf5bb56e004a5dd82006f6bf3bf4b30d0affb2
unknown <sgregory@gmail.com>
2014-11-23 11:23:19 -0600

    Added a few more dishes and type to items

--- app/core/items.js
+++ app/core/items.js
@@ -2,14 +2,37 @@ define(function() {
     'use strict';
 
     var items = [{
-        name: "Tom Yum Goong",
-        price: "5.99"
+        name: 'Soft Spring Rolls',
+        price: 4.99,
+        type: 'appetizer'
     }, {
-        name: "Soft Spring Rolls",
-        price: "4.99"
+        name: 'Satay Chicken',
+        price: 6.99,
+        type: 'appetizer'
     }, {
-        name: "Pad Thai",
-        price: "8.99"
+        name: 'Tom Yum Chicken',
+        price: 4.99,
+        type: 'soup'
+    }, {
+        name: 'Tom Yum Goong',
+        price: 5.99,
+        type: 'soup'
+    }, {
+        name: 'Pad Thai',
+        price: 8.99,
+        type: 'entree'
+    }, {
+        name: 'Pad Kee Mao',
+        price: 8.99,
+        type: 'entree'
+    }, {
+        name: 'Pad Khing',
+        price: 9.99,
+        type: 'entree'
+    }, {
+        name: 'Angry Beef',
+        price: 9.99,
+        type: 'entree'
     }];
 
     return items;
