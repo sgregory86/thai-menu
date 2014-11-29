8d9382f2562469fe984a961baa05024c7ee3a9b9
unknown <sgregory@gmail.com>
2014-11-29 15:41:48 -0600

    Added Gulp to the project and updated bower package

--- README.md
+++ README.md
@@ -6,9 +6,3 @@ Thai Menu
 A dinner menu SPA using Knockout and RequireJS with a focus on KO components.
 
 >Just got this thing kicked off.
-
-## Creator
-
-**Steven Gregory**
-
-- <http://twitter.com/sgregory>
---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp'),
+    plug = require('gulp-load-plugins')();
+
+gulp.task('htmlhint', function() {
+    return gulp.src('index.html')
+        .pipe(plug.htmlhint())
+        .pipe(plug.htmlhint.reporter());
+});
+
+gulp.task('jshint', function() {
+    return gulp.src([
+            'app/**/*.js',
+            '!bower_components/**'
+        ])
+        .pipe(plug.jshint())
+        .pipe(plug.jshint.reporter());
+});
+
+gulp.task('watch', function() {
+    gulp.watch('index.html', ['htmlhint']);
+    gulp.watch('app/**/*.js', ['jshint']);
+});
+
+gulp.task('default', ['htmlhint', 'jshint', 'watch']);
